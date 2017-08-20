7f764951de43b9108d0960da0e2c0cffa350ca58
Vadym <sukolenvo@gmail.com>
2017-08-20 23:42:04 +0300

    ad free period

--- app/src/main/java/com/dakare/radiorecord/app/ads/AdUtils.java
+++ app/src/main/java/com/dakare/radiorecord/app/ads/AdUtils.java
@@ -1,18 +1,37 @@
 package com.dakare.radiorecord.app.ads;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.view.View;
 import com.dakare.radiorecord.app.PreferenceManager;
+import com.dakare.radiorecord.app.RecordApplication;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
+import java.util.concurrent.TimeUnit;
+
 public class AdUtils {
 
+    private static long AD_FREE_PERIOD;
+
+    static {
+        try {
+            AD_FREE_PERIOD = RecordApplication.getInstance()
+                    .getPackageManager()
+                    .getPackageInfo(RecordApplication.getInstance().getPackageName(), 0)
+                    .firstInstallTime + TimeUnit.DAYS.toMillis(3);
+        } catch (PackageManager.NameNotFoundException e) {
+            //Nothing to do
+        }
+    }
+
     public static void showAd(final AdView adView) {
-        if (PreferenceManager.getInstance(adView.getContext()).getShowAd() && !isMobileConnection(adView.getContext())) {
+        if (AD_FREE_PERIOD < System.currentTimeMillis()
+                && PreferenceManager.getInstance(adView.getContext()).getShowAd()
+                && !isMobileConnection(adView.getContext())) {
             AdRequest adRequest = new AdRequest.Builder()
                     .addTestDevice("9740472D383B4180A2C2E09C23C160FC")
                     .build();
