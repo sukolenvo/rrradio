14d1b2ef6b82338de3c5efe5d1dfe901bd2deec0
vadym <sukolenvo@gmail.com>
2018-05-21 21:34:23 +0300

    start foreground service

--- app/src/main/java/com/dakare/radiorecord/app/player/listener/controls/ControlReceiver.java
+++ app/src/main/java/com/dakare/radiorecord/app/player/listener/controls/ControlReceiver.java
@@ -3,6 +3,7 @@ package com.dakare.radiorecord.app.player.listener.controls;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.KeyEvent;
 import com.dakare.radiorecord.app.player.listener.NotificationListener;
@@ -38,7 +39,7 @@ public class ControlReceiver extends BroadcastReceiver {
                 if (action != null) {
                     Intent service = new Intent(context, PlayerService.class);
                     service.setAction(action);
-                    context.startService(service);
+                    ContextCompat.startForegroundService(context, intent);
                 }
             }
         }
